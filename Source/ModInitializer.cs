206c141171e62cdc0f24f38d1178c716b593d7c2
UnlimitedHugs <symbolic@land.ru>
2016-01-17 21:10:59 +0400

    Basic reroll functionality

---
+++ Source/ModInitializer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Verse;
+using Object = UnityEngine.Object;
+
+namespace MapReroll {
+	public class ModInitializer : ITab {
+		
+		private static GameObject obj;
+
+		public ModInitializer(){
+			if (obj != null) return;
+			obj = new GameObject("MapRerollLoader");
+			obj.AddComponent<ModInitializerComponent>();
+			Object.DontDestroyOnLoad(obj);
+		}
+		
+		protected override void FillTab() {			
+		}
+	}
+}
---
+++ Source/ModInitializerComponent.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using RimWorld;
+using RimWorld.Planet;
+using UnityEngine;
+using Verse;
+using System.Linq;
+
+namespace MapReroll {
+	public class ModInitializerComponent : MonoBehaviour {
+		private static bool injectionPerformed;
+		private static bool popped;
+
+		public void FixedUpdate() {
+			if (popped) return;
+			if(Input.GetKeyDown(KeyCode.Keypad5)) {
+				popped = true;
+				Action newEventAction = delegate {
+					var pawns = Find.ListerPawns.AllPawns.ToList();
+					foreach (var pawn in pawns) {
+						if(pawn.Faction == Faction.OfColony && pawn.SpawnedInWorld) {
+							pawn.DeSpawn();
+						}
+					}
+					/*var elev = MapGenerator.FloatGridNamed("Elevation");
+
+					foreach (var cell in Find.Map.AllCells) {
+						if(Rand.Range(0, 10)<=1) {
+							elev[cell] = 1;
+						}
+					}*/
+					Find.World.info.seedString = Rand.Int.ToString();
+					MapInitData.mapToLoad = null;
+					Application.LoadLevel("Gameplay");
+				};
+				LongEventHandler.QueueLongEvent(newEventAction, "LoadingLongEvent".Translate());
+				
+			}
+			if(injectionPerformed || Game.Mode != GameMode.MapPlaying) return;
+			injectionPerformed = true;
+
+			//var sec = Find.MapDrawer.SectionAt(new IntVec3());
+			//var layersProp = typeof(Section).GetField("layers", BindingFlags.NonPublic | BindingFlags.Instance);
+			//var layers = (List<SectionLayer>)layersProp.GetValue(sec);
+			
+
+			/*var sec = Find.MapDrawer.SectionAt(new IntVec3());
+			var layer = sec.GetLayer(typeof(TestSection));
+			Log.Message(layer.ToString());*/
+		}
+
+		public void OnLevelWasLoaded() {
+			injectionPerformed = false;
+			popped = false;
+		}
+	}
+}
