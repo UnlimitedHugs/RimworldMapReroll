deb75d4514f45746be143e4f6a460e83dd9c7b82
UnlimitedHugs <symbolic@land.ru>
2016-04-29 01:40:38 +0400

    1.1.3: Added Prepare Carefully (A13) compatibility patch

---
+++ Source/PrepareCarefullyCompat.cs
@@ -0,0 +1,21 @@
+﻿using EdB.PrepareCarefully;
+using Verse;
+
+namespace MapReroll {
+	/**
+	 * This is a temporary compatibility fix for the early release of Prepare Carefully for A13
+	 */
+	public class PrepareCarefullyCompat {
+		public static readonly string ModName = "EdB Prepare Carefully";
+
+		public void Notify_OnLevelLoaded() {
+			// Prepare Carefully might already be fixed- in that case do nothing
+			if (PrepareCarefully.Instance.Colonists.Count>0) return;
+			if(!PrepareCarefully.Instance.Active) return;
+			// Restore colonist entries. MapInitData.colonists were restored in MapRerollController
+			foreach (var pawn in MapInitData.colonists) {
+				PrepareCarefully.Instance.Colonists.Add(pawn);
+			}
+		}
+	}
+}
